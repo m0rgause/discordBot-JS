452f79a28b56bf676638486d5fcde50992617fa2
kiki-af <antoresf1@gmail.com>
2022-11-08 01:42:14 +0700

    Fix app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -41,7 +41,7 @@ module.exports.launch = async (client) => {
         }),
       })
     ) // Set the express session password and configuration
-        name: "connection",})) // Set the express session password and configuration
+    // Set the express session password and configuration
     .use(async function (req, res, next) {
       req.user = req.session.user;
       req.client = client;
